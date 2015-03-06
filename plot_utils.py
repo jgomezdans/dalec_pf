c4d17392a1e4e6a3e2f4a33e5f4f0c9389a2d894
Jose Gomez-Dans <jgomezdans@gmail.com>
2015-03-06 13:44:52 +0000

    Added pretty plot utils

---
+++ plot_utils.py
@@ -0,0 +1,51 @@
+#!/usr/bin/env python
+import json
+import numpy as np
+import matplotlib.pyplot as plt
+
+def plot_config ():
+    """Update the MPL configuration"""
+    config_json='''{
+            "lines.linewidth": 2.0,
+            "axes.edgecolor": "#bcbcbc",
+            "patch.linewidth": 0.5,
+            "legend.fancybox": true,
+            "axes.color_cycle": [
+                "#66C2A5",
+                "#FC8D62",
+                "#8DA0CB",
+                "#E78AC3",
+                "#A6D854",
+                "#FFD92F",
+                "#E5C494",
+                "#B3B3B3"
+            ],
+            "axes.facecolor": "#eeeeee",
+            "axes.labelsize": "large",
+            "axes.grid": false,
+            "patch.edgecolor": "#eeeeee",
+            "axes.titlesize": "x-large",
+            "svg.embed_char_paths": "path",
+            "xtick.direction" : "out",
+            "ytick.direction" : "out"
+    }
+    '''
+    s = json.loads ( config_json )
+    plt.rcParams.update(s)
+    
+
+def pretty_axes( ax ):
+    """This function takes an axis object ``ax``, and makes it purrty.
+    Namely, it removes top and left axis & puts the ticks at the
+    bottom and the left"""
+
+    ax.spines["top"].set_visible(False)  
+    ax.spines["bottom"].set_visible(True)  
+    ax.spines["right"].set_visible(False)              
+    ax.spines["left"].set_visible(True)  
+
+    ax.get_xaxis().tick_bottom()  
+    ax.get_yaxis().tick_left()  
+
+    ax.tick_params(axis="both", which="both", bottom="off", top="off",  
+            labelbottom="on", left="off", right="off", labelleft="on")  
