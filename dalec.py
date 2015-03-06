84bb10a9a185e4f1ddc3f2a049a2d57b9c1d7c50
Jose Gomez-Dans <jgomezdans@gmail.com>
2015-03-06 12:45:38 +0000

    Nice plots for the DALEC run

--- dalec.py
+++ dalec.py
@@ -14,7 +14,41 @@ def acm ( lat, lai, doy, tmx, tmn, irad,  ca, nitrogen, \
             psid=-2., rtot=1.):
     """
     The ACM model. The default parameters in ``a`` are taken from
-    Williams et al (2005), 
+    Williams et al (2005), and are an option. Otherwise, the ACM model
+    takes a number of meteo and evironmental inputs. For a full description
+    of the model, see Williams et al (1997) 
+    http://www.geosciences.ed.ac.uk/homes/mwilliam/WilliamsEA97.pdf
+    or the appendix of Williams et al (2005).
+    
+    Parameters
+    -----------
+    lat: float
+        Latitude of the site in decimal degrees [deg]
+    lai: float
+        The leaf area index (LAI) [m^2/m^2]
+    doy: int
+        The Day of year [day]
+    tmx: float
+        Maximum daily temperature [degC]
+    tmn: float
+        Minimum daily temperature [degC]
+    irad: float
+        Irradiance [MJ m^{-2} day^{-1}].
+    ca: float
+        CO2 concentration [ppm]
+    nitrogen: float
+        Average leaf nitrogen [gNm^{2}leaf area]
+    a: array
+        ACM parameters
+    psid: float
+        Maximum soil-leaf water potential difference [MPa]
+    rtot: float
+        Total plant-soil hydraulic resistance [MPa m^2 s mmol^{-1}]
+    
+    Returns
+    ---------
+    GPP
+    
     """
     tmpr = 0.5 * (tmx - tmn )
     
@@ -52,9 +86,52 @@ def dalec ( doy, tmn, tmp, tmx, irad, ca, nitrogen, \
     
     This function is the daily update of the model. It takes a number of drivers
     as well as the state of the C pools, and some parameters that control rates
-    and stuff.
+    and stuff. In order to calculate GPP, it uses the ACM model, which is in
+    a different function, called ``acm``.
     
+    Parameters
+    -----------
     
+    doy: int
+        The Day of year [day]
+    tmn: float
+        Minimum daily temperature [degC]
+    tmx: float
+        Maximum daily temperature [degC]
+    irad: float
+        Irradiance [MJ m^{-2} day^{-1}].    
+    ca: float
+        CO2 concentration [ppm]
+    nitrogen: float
+        Average leaf nitrogen [gNm^{2}leaf area]
+    lat: float
+        Latitude of the site in decimal degrees [deg]
+    lma: float
+        Leaf mass per unit area [gm^2leaf area]
+    p: array
+        Parameter vector
+    Cf: float
+        Foliar C pool (gCm^2)
+    Cr: float
+        Root C pool (gCm^2)
+    Cw: float
+        Woody C pool (gCm^2)
+    Clit: float
+        Litter C pool (gCm^2)
+    Csom: float
+        SOM C pool (gCm^2)
+    psid: float
+        Maximum soil-leaf water potential difference [MPa]
+    rtot: float
+        Total plant-soil hydraulic resistance [MPa m^2 s mmol^{-1}]
+    a: array
+        ACM parameters
+    lai: float (optional)
+        You might drive the model directly with LAI, rather than with Cf/lma...
+        
+    Returns
+    ---------
+    NEE, GPP, Cf, Cr, Cw, Clit, Csom, LAI
     """
     if lai is None:
         lai = max ( 0.1, Cf/lma ) # LAI based on foliar pool
@@ -87,9 +164,9 @@ def dalec ( doy, tmn, tmp, tmx, irad, ca, nitrogen, \
     
     nee = Ra+Rh1+Rh2-gpp
     
-    return ( nee, gpp, Cf, Cr, Cw, Clit, Csom, lai )
+    return ( nee, gpp, Ra, Rh1, Rh2, Cf, Cr, Cw, Clit, Csom, lai )
     
-if __name__ == "__main__":
+def test_dalec():
     # Meteolius First Young Pine
     driver_data = np.loadtxt ( "dalec_drivers.OREGON.no_obs.dat" )
     doys = driver_data[ :, 0 ] 
@@ -113,16 +190,63 @@ if __name__ == "__main__":
     p_vect=np.array([ 4.41e-6, 0.47, 0.3150, 0.4344,0.002665, 2.06e-6, 2.48e-3, \
                 2.28e-2, 2.65e-6 ] )
 
-    GPP = []
-    NEE = []
-    CF = []
+    outputs = np.zeros( (11, len (doys )) )
     for (i, doy ) in enumerate ( doys ) :
-        ( nee, gpp, Cf, Cr, Cw, Clit, Csom, lai ) = dalec ( doy, \
+        ( nee, gpp, Ra, Rh1, Rh2, Cf, Cr, Cw, Clit, Csom, lai ) = dalec ( doy, \
             tmn[i], tmp[i], tmx[i], irad[i], ca[i], nitrogen[i], \
             lat, lma, p_vect, \
             Cf, Cr, Cw, Clit, Csom, psid=psid[i], rtot=rtot[i] )
-        NEE.append ( nee )
-        CF.append ( Cf )
-        
-    plt.plot( NEE)
-    plt.show()
+        outputs[:, i] = ( nee, gpp, Ra, Rh1, Rh2, Cf, Cr, Cw, Clit, Csom, lai )
+    tx = np.arange ( len(doys))
+    plt.figure(figsize=(8,8))
+    plt.plot ( tx, outputs[0,:], '-', label="NEE" )
+    plt.plot ( tx, outputs[1,:], '-', label="GPP" )
+    plt.plot ( tx, outputs[2,:], '-', label="Ra" )
+    plt.plot ( tx, outputs[3,:], '-', label="Rh1" )
+    plt.plot ( tx, outputs[4,:], '-', label="Rh2" )
+    plt.plot ( tx, tx*0., '-', lw=1.5, c="0.8" )
+    plt.xlabel("Days after 01/01/2000")
+    plt.ylabel ("Flux magnitude $[gCm^{-2}d^{-1}]$" )
+    plt.title ( "DALEC calculated fluxes" )
+    plt.legend ( loc="best", fancybox=True )
+    ax = plt.gca()
+    ax.spines["top"].set_visible(False)  
+    ax.spines["bottom"].set_visible(True)  
+    ax.spines["right"].set_visible(False)  
+    
+    ax.spines["left"].set_visible(True)  
+
+    #Ensure that the axis ticks only show up on the bottom and left of the plot.  
+    #Ticks on the right and top of the plot are generally unnecessary chartjunk.  
+    ax.get_xaxis().tick_bottom()  
+    ax.get_yaxis().tick_left()  
+
+    ax.tick_params(axis="both", which="both", bottom="off", top="off",  
+            labelbottom="on", left="off", right="off", labelleft="on")  
+
+    pools = [r'$C_f$',r'$C_r$',r'$C_w$',r'$C_{lit}$',r'$C_{SOM}$']
+    fig, axs = plt.subplots (nrows=2, ncols=3, figsize=(10,8) )
+    
+    for i, ax in enumerate(axs.flatten() ):
+        if i < 5:
+            ax.plot ( tx, outputs[5+i,:], '-' )
+            ax.set_title (pools[i], fontsize=12 )
+            ax.spines["top"].set_visible(False)  
+            ax.spines["bottom"].set_visible(True)  
+            ax.spines["right"].set_visible(False)  
+            
+            ax.spines["left"].set_visible(True)  
+
+            # Ensure that the axis ticks only show up on the bottom and left of the plot.  
+            # Ticks on the right and top of the plot are generally unnecessary chartjunk.  
+            ax.get_xaxis().tick_bottom()  
+            ax.get_yaxis().tick_left()  
+
+            ax.tick_params(axis="both", which="both", bottom="off", top="off",  
+                    labelbottom="on", left="off", right="off", labelleft="on")  
+            
+            
+        else:
+            ax.set_visible ( False )
+        ax.set_xlim ( 0, 1100 )
+    plt.subplots_adjust ( wspace=0.3 )
